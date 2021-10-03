7c3db25365fa713261934384e930d42fb0d881d1
Mathias Lang <39031482+m4dmin@users.noreply.github.com>
2021-10-03 13:43:20 +0200

    Add files via upload

--- python/aktuell_read-proxon-data-write-to-influxdb.py
+++ python/aktuell_read-proxon-data-write-to-influxdb.py
@@ -27,11 +27,11 @@ influxDB_db = config['influxDB']['db']
 influxDB_tag_instance = config['influxDB']['tag_instance']
 influxDB_tag_source = config['influxDB']['tag_source']
 
-serial_port = config['serial']['port']
+serial_port = config['modbus']['port']
 
 ##### Proxon section
 try:
-    instr = minimalmodbus.Instrument("/dev/ttyUSB0", 41)
+    instr = minimalmodbus.Instrument(serial_port, 41)
     instr.serial.baudrate = 19200 
     instr.serial.bytesize = 8
     instr.serial.parity   = minimalmodbus.serial.PARITY_EVEN
@@ -45,27 +45,27 @@ points = []
 
 ##### Array with Modbus registers
         #Read Holding Register # FC4 = 3
-reg =  [[  16, 0, 3, 'wp_modus_betriebsart' ,'Betriebsart'],
-        [  62, 0, 3, 'wp_on-off_kuehlung' ,'Kühlung an/aus'],
-        [  70, 2, 3, 'wp_soll-temp_zone1' ,'Wohnen  (Zone1) Soll-Temperatur'],
-        [  75, 2, 3, 'wp_soll-temp_zone2' ,'Büro KG (Zone2) Soll-Temperatur'],
-        [ 133, 0, 3, 'wp_restzeit_intensivlueftung' ,'Intensivlüftung Restzeit'],
-        [2001, 0, 3, 't300_on-off_heizstab' ,'Heizstab an/aus'],
-        [2000, 1, 3, 't300_soll-temp_wasser' ,'Wasser Soll-Temperatur'],
-        [2003, 1, 3, 't300_schwelle-temp_wasser' ,'Wasser Temperatur-Schwelle Heizstab'],
+reg =  [[  16, 0, 3, 'wp_modus_betriebsart', 'mode', 'Betriebsart'], # 0=Aus, 1=EcoSommer, 2=EcoWinter, 3=Komfort, 9=Test
+        [  62, 0, 3, 'wp_on-off_kuehlung', 'switch', 'Kühlung an/aus'], # 0=Aus, 1=An
+        [  70, 2, 3, 'wp_soll-temp_zone1', 'C', 'Wohnen  (Zone1) Soll-Temperatur'], # -0,5 Abweichung zur Anzeige in der Anlage
+        [  75, 2, 3, 'wp_soll-temp_zone2', 'C', 'Büro KG (Zone2) Soll-Temperatur'],
+        [ 133, 0, 3, 'wp_restzeit_intensivlueftung', 'min', 'Intensivlüftung Restzeit'],
+        [2001, 0, 3, 't300_on-off_heizstab', 'switch', 'Heizstab an/aus'], # 0=Aus, 1=An
+        [2000, 1, 3, 't300_soll-temp_wasser', 'C', 'Wasser Soll-Temperatur'],
+        [2003, 1, 3, 't300_schwelle-temp_wasser', 'C', 'Wasser Temperatur-Schwelle Heizstab'],
         #Read Input Register   # FC3 = 4
-        [ 814, 1, 4, 't300_temp_wasser' ,'Wasser Temperatur'],
-        [  41, 2, 4, 'wp_temp_zone1' ,'Wohnen  (Zone1) Temperatur'],
-        [  40, 2, 4, 'wp_temp_zone2' ,'Büro KG (Zone2) Temperatur'],
-        [ 593, 1, 4, 'wp_temp_kochen' ,'Kochen Temperatur'],
-        [ 596, 1, 4, 'wp_temp_diele' ,'Diele Temperatur'],
-        [ 599, 1, 4, 'wp_temp_buero-eg' ,'Büro EG Temperatur'],
-        [ 602, 1, 4, 'wp_temp_schalfen' ,'Schlafen Temperatur'],
-        [ 605, 1, 4, 'wp_temp_martha' ,'Martha Temperatur'],
-        [ 608, 1, 4, 'wp_temp_marlene' ,'Marlene Temperatur'],
-        [ 614, 1, 4, 'wp_temp_keller2' ,'Keller 2 Temperatur'],
-        [ 617, 1, 4, 'wp_temp_keller3' ,'Keller 3 Temperatur'],
-        [ 154, 0, 4, 'wp_stufe_ventilator-zuluft' ,'Ventilator Zuluft Lüftungsstufe']]    
+        [ 814, 1, 4, 't300_temp_wasser', 'C', 'Wasser Temperatur'], # +100 Abweichung zur tatsächlichen Temperatur
+        [  41, 2, 4, 'wp_temp_zone1', 'C', 'Wohnen  (Zone1) Temperatur'],
+        [  40, 2, 4, 'wp_temp_zone2', 'C', 'Büro KG (Zone2) Temperatur'],
+        [ 593, 1, 4, 'wp_temp_kochen', 'C', 'Kochen Temperatur'],
+        [ 596, 1, 4, 'wp_temp_diele', 'C', 'Diele Temperatur'],
+        [ 599, 1, 4, 'wp_temp_buero-eg', 'C', 'Büro EG Temperatur'],
+        [ 602, 1, 4, 'wp_temp_schalfen', 'C', 'Schlafen Temperatur'],
+        [ 605, 1, 4, 'wp_temp_martha', 'C', 'Martha Temperatur'],
+        [ 608, 1, 4, 'wp_temp_marlene', 'C', 'Marlene Temperatur'],
+        [ 614, 1, 4, 'wp_temp_keller2', 'C', 'Keller 2 Temperatur'],
+        [ 617, 1, 4, 'wp_temp_keller3', 'C', 'Keller 3 Temperatur'],
+        [ 154, 0, 4, 'wp_stufe_ventilator-zuluft', 'level', 'Ventilator Zuluft Lüftungsstufe']]    
 
 
 ############### logging section ###############
@@ -84,7 +84,7 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 # logging examples
- logger.debug("Debug Log")
+logger.debug("Debug Log")
 # logger.info("Info Log")
 # logger.warning("Warning Log")
 # logger.error("Error Log")
@@ -98,37 +98,35 @@ try:
     logger.debug("0000 - Starte Lesevorgang")
 
     for i in range(len(reg)):
+        logger.debug("Register Array - "+str(reg[i]))
 
+        value = instr.read_register(reg[i][0],reg[i][1],reg[i][2],True)
+        logger.debug("Value: "+str(value))
 
+        if reg[i][3] == 'wp_soll-temp_zone1':
+            value = value + 0.5
+            logger.debug("Value: "+str(value))
 
-        logger.debug("Register "+str(i)+" einlesen")
-        sz_responseByte = ser.readline()
-        logger.debug(sz_responseByte)
-        sz_responseString = sz_responseByte.decode("utf-8").rstrip('\r\n')
+        if reg[i][3] == 't300_temp_wasser':
+            value = value - 100
+            logger.debug("Value: "+str(value))
 
-        # sz_leistung_aktuell
-        if sz_responseString[0:len(sz_leistung_aktuell)] == sz_leistung_aktuell:
-            sz_responseStripped = sz_responseString.replace(sz_leistung_aktuell,"")
-            sz_responseStripped = sz_responseStripped.replace("(","")
-            sz_responseStripped = sz_responseStripped.replace("*kW)","")
-            sz_responseStrippedW = float(sz_responseStripped)*1000
-            sz_actW = sz_responseStrippedW
-            logger.info("SZ Leistung aktuell: "+str(sz_actW))
-            point = {
-                "measurement": 'sz_leistung_aktuell',
-                "tags": {
-                    "instance": influxDB_tag_instance,
-                    "source": influxDB_tag_source
-                },
-                #   "time": timestamp,   # Wenn nicht genutzt, wird der aktuelle Timestamp aus influxDB genutzt
-                "fields": {
-                    "W": sz_responseStrippedW
-                    }
+        
+
+        logger.info(str(reg[i][5]).ljust(35)+": "+str(value)+" "+str(reg[i][4]))
+        point = {
+            "measurement": str(reg[i][3]),
+            "tags": {
+                "instance": influxDB_tag_instance,
+                "source": influxDB_tag_source
+            },
+            #   "time": timestamp,   # Wenn nicht genutzt, wird der aktuelle Timestamp aus influxDB genutzt
+        "fields": {
+                str(reg[i][4]): value
                 }
-            points.append(point)
-
-
-
+            }
+        logger.debug(str(point))
+        points.append(point)
 
 except Exception as e:
     logger.error(e)
