87165e6d26ba5baa890a2951c29d4036099e769f
Mathias Lang <39031482+m4dmin@users.noreply.github.com>
2021-10-19 15:16:34 +0200

    Add files via upload

--- python/aktuell_read-proxon-data-write-to-influxdb.py
+++ python/aktuell_read-proxon-data-write-to-influxdb.py
@@ -45,14 +45,14 @@ except Exception as e:
     sys.exit(1)
 
 ##### Process Lock section
-unlocked = False
-while unlocked == False:
+locked = True
+while unlocked == True:
     try:
         logger.debug("Lock schleife start")
         lock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         logger.debug("Lock schleife bind")
         lock.bind( '\0postconnect_gateway_notify_lock') 
-        unlocked = True
+        locked = False
 
     except socket.error as e:
         logger.debug(e)
@@ -77,7 +77,6 @@ try:
     influxDB_tag_source = config['influxDB']['tag_source']
 
     serial_port = config['modbus']['port']
-    slave_address = config['modbus']['port']
    
     mqtt_broker_address = config['mqtt']['broker_address']
     mqtt_client_name = str(socket.gethostname()+sys.argv[0])
@@ -107,28 +106,31 @@ try:
     points = []
 
     # Modbus registers
-        #Read Holding Register # FC4 = 3
-    reg =  [[  16, 0, 3, 'wp_modus_betriebsart',            'mode',     'Betriebsart'],                         # 0=Aus, 1=EcoSommer, 2=EcoWinter, 3=Komfort, 9=Test
-            [  62, 0, 3, 'wp_on-off_kuehlung',              'switch',   'Kühlung an/aus'],                      # 0=Aus, 1=An
-            [  70, 2, 3, 'wp_soll-temp_zone1',              'temp',     'Wohnen  (Zone1) Soll-Temperatur'],     # -0,5 Abweichung zur Anzeige in der Anlage
-            [  75, 2, 3, 'wp_soll-temp_zone2',              'temp',     'Büro KG (Zone2) Soll-Temperatur'],
-            [ 133, 0, 3, 'wp_restzeit_intensivlueftung',    'time',     'Intensivlüftung Restzeit'],
-            [2001, 0, 3, 't300_on-off_heizstab',            'switch',   'Heizstab an/aus'],                     # 0=Aus, 1=An
-            [2000, 1, 3, 't300_soll-temp_wasser',           'temp',     'Wasser Soll-Temperatur'],
-            [2003, 1, 3, 't300_schwelle-temp_wasser',       'temp',     'Wasser Temperatur-Schwelle Heizstab'],
+    # - write_register(registeraddress: int, value: Union[int, float], number_of_decimals: int = 0, functioncode: int = 16, signed: bool = False)
+    # - read_register(registeraddress: int, number_of_decimals: int = 0, functioncode: int = 3, signed: bool = False)
+    #        reg-nr, nr-decimals-read, nr-decimals-write, functioncode, signed,             device/measurement, type (HA), comment
+            #Read Holding Register # FC4 = 3
+    reg =  [[    16,                0,                 0,            3,   True,         'wp_modus_betriebsart',    'mode', 'Betriebsart'],                         # 0=Aus, 1=EcoSommer, 2=EcoWinter, 3=Komfort
+            [    62,                0,                 0,            3,     '',           'wp_on-off_kuehlung',  'switch', 'Kühlung an/aus'],                      # 0=Aus, 1=An
+            [  2001,                0,                 0,            3,   True,         't300_on-off_heizstab',  'switch', 'Heizstab an/aus'],                     # 0=Aus, 1=An
+            [    70,                2,                 1,            3,   True,           'wp_soll-temp_zone1',    'temp', 'Wohnen  (Zone1) Soll-Temperatur'],     # 100 - 295  ##-0,5 Abweichung zur Anzeige in der Anlage
+            [    75,                2,                 1,            3,   True,           'wp_soll-temp_zone2',    'temp', 'Büro KG (Zone2) Soll-Temperatur'],     # 100 - 295
+            [  2000,                1,                 1,            3,   True,        't300_soll-temp_wasser',    'temp', 'Wasser Soll-Temperatur'],              # 450 - 600
+            [  2003,                1,                 1,            3,   True,    't300_schwelle-temp_wasser',    'temp', 'Wasser Temperatur-Schwelle Heizstab'], # 400 - 500
+            [   133,                0,                 0,            3,   True, 'wp_restzeit_intensivlueftung',    'time', 'Intensivlüftung Restzeit'],            # 0 - 1440
             #Read Input Register   # FC3 = 4
-            [ 814, 1, 4, 't300_temp_wasser',                'temp',     'Wasser Temperatur'],                   # +100 Abweichung zur tatsächlichen Temperatur
-            [  41, 2, 4, 'wp_temp_zone1',                   'temp',     'Wohnen  (Zone1) Temperatur'],
-            [  40, 2, 4, 'wp_temp_zone2',                   'temp',     'Büro KG (Zone2) Temperatur'],
-            [ 593, 1, 4, 'wp_temp_kochen',                  'temp',     'Kochen Temperatur'],
-            [ 596, 1, 4, 'wp_temp_diele',                   'temp',     'Diele Temperatur'],
-            [ 599, 1, 4, 'wp_temp_buero-eg',                'temp',     'Büro EG Temperatur'],
-            [ 602, 1, 4, 'wp_temp_schlafen',                'temp',     'Schlafen Temperatur'],
-            [ 605, 1, 4, 'wp_temp_martha',                  'temp',     'Martha Temperatur'],
-            [ 608, 1, 4, 'wp_temp_marlene',                 'temp',     'Marlene Temperatur'],
-            [ 614, 1, 4, 'wp_temp_keller2',                 'temp',     'Keller 2 Temperatur'],
-            [ 617, 1, 4, 'wp_temp_keller3',                 'temp',     'Keller 3 Temperatur'],
-            [ 154, 0, 4, 'wp_stufe_ventilator-zuluft',      'level',    'Ventilator Zuluft Lüftungsstufe']]     # Stufen 1 bis 4
+            [   814,                1,                '',            4, 'none',             't300_temp_wasser',     'temp', 'Wasser Temperatur'],                  # +100 Abweichung zur tatsächlichen Temperatur
+            [    41,                2,                '',            4, 'none',                'wp_temp_zone1',     'temp', 'Wohnen  (Zone1) Temperatur'],
+            [    40,                2,                '',            4, 'none',                'wp_temp_zone2',     'temp', 'Büro KG (Zone2) Temperatur'],
+            [   593,                1,                '',            4, 'none',               'wp_temp_kochen',     'temp', 'Kochen Temperatur'],
+            [   596,                1,                '',            4, 'none',                'wp_temp_diele',     'temp', 'Diele Temperatur'],
+            [   599,                1,                '',            4, 'none',             'wp_temp_buero-eg',     'temp', 'Büro EG Temperatur'],
+            [   602,                1,                '',            4, 'none',             'wp_temp_schlafen',     'temp', 'Schlafen Temperatur'],
+            [   605,                1,                '',            4, 'none',               'wp_temp_martha',     'temp', 'Martha Temperatur'],
+            [   608,                1,                '',            4, 'none',              'wp_temp_marlene',     'temp', 'Marlene Temperatur'],
+            [   614,                1,                '',            4, 'none',              'wp_temp_keller2',     'temp', 'Keller 2 Temperatur'],
+            [   617,                1,                '',            4, 'none',              'wp_temp_keller3',     'temp', 'Keller 3 Temperatur'],
+            [   154,                0,                '',            4, 'none',   'wp_stufe_ventilator-zuluft',    'level', 'Ventilator Zuluft Lüftungsstufe']]     # Stufen 1 bis 4
 
 except Exception as e:
     logger.debug(e)
@@ -142,6 +144,47 @@ def on_connect(client, userdata, flags, rc):
     try:
         logger.info("Connected with result code "+str(rc))
         client.publish(mqtt_topic_debug,"Devive "+str(client_name)+" connected with result code "+str(rc))
+
+        ##### Proxon section
+        logger.debug("Starte Lesevorgang")
+
+        for i in range(len(reg)):
+            mqtt_state_topic = mqtt_topic_prefix+"/"+str(reg[i][6])+"/"+str(reg[i][5])+"/state"
+            logger.debug("state_topic - "+mqtt_state_topic)
+            logger.debug("Register Array - "+str(reg[i]))
+            value = instr.read_register(reg[i][0],reg[i][1],reg[i][3],True)
+            logger.debug("Value: "+str(value))
+
+            if reg[i][5] == 'wp_soll-temp_zone1':
+                value = value + 0.5
+                logger.debug("Value: "+str(value))
+
+            if reg[i][5] == 't300_temp_wasser':
+                value = value - 100
+                logger.debug("Value: "+str(value))
+
+            client.publish(mqtt_state_topic,str(value),qos=0,retain=True)
+
+            logger.info(str(reg[i][7]).ljust(35)+": "+str(value)+" "+str(reg[i][6]))
+            point = {
+                "measurement": str(reg[i][5]),
+                "tags": {
+                    "instance": influxDB_tag_instance,
+                    "source": influxDB_tag_source
+                },
+                #   "time": timestamp,   # Wenn nicht genutzt, wird der aktuelle Timestamp aus influxDB genutzt
+                "fields": {
+                    str(reg[i][6]): value
+                    }
+                }
+            logger.debug(str(point))
+            points.append(point)
+
+        clientInfluxDB.write_points(points)
+
+        lock.close()
+        logger.info("Closed running processes")
+
     except Exception as e:
         logger.debug(e)
         sys.exit(2)
@@ -149,53 +192,16 @@ def on_connect(client, userdata, flags, rc):
 
 ##### Runtime section ------------------------------------------------------------------------------------------------------------------------------------------
 try:
+    ##### influxDB section
+    clientInfluxDB = InfluxDBClient(influxDB_ip, influxDB_port, influxDB_user, influxDB_passwd, influxDB_db)
+    
     ##### MQTT section
     logger.info("Creating new MQTT instance")
     client = mqtt.Client(mqtt_client_name)
     client.on_connect = on_connect
     logger.info("Connection to MQTT broker")
     client.connect(mqtt_broker_address)
-    
-    ##### Proxon section
-    logger.debug("0000 - Starte Lesevorgang")
-
-    for i in range(len(reg)):
-        mqtt_state_topic = mqtt_topic_prefix+"/"+str(reg[i][4])+"/"+str(reg[i][3])+"/state_topic"
-        logger.debug("state_topic - "+mqtt_state_topic)
-
-        logger.debug("Register Array - "+str(reg[i]))
-
-        value = instr.read_register(reg[i][0],reg[i][1],reg[i][2],True)
-        logger.debug("Value: "+str(value))
-
-        if reg[i][3] == 'wp_soll-temp_zone1':
-            value = value + 0.5
-            logger.debug("Value: "+str(value))
-
-        if reg[i][3] == 't300_temp_wasser':
-            value = value - 100
-            logger.debug("Value: "+str(value))
-
-        client.publish(mqtt_state_topic,str(value))
-
-        logger.info(str(reg[i][5]).ljust(35)+": "+str(value)+" "+str(reg[i][4]))
-        point = {
-            "measurement": str(reg[i][3]),
-            "tags": {
-                "instance": influxDB_tag_instance,
-                "source": influxDB_tag_source
-            },
-            #   "time": timestamp,   # Wenn nicht genutzt, wird der aktuelle Timestamp aus influxDB genutzt
-        "fields": {
-                str(reg[i][4]): value
-                }
-            }
-        logger.debug(str(point))
-        points.append(point)
-
-    ##### influxDB section
-    clientInfluxDB = InfluxDBClient(influxDB_ip, influxDB_port, influxDB_user, influxDB_passwd, influxDB_db)
-    clientInfluxDB.write_points(points)
+    logger.info("Closed connection to MQTT broker")
 
 except Exception as e:
     logger.error(e)
--- python/mqtt-listener_write_proxon_influxdb.py
+++ python/mqtt-listener_write_proxon_influxdb.py
@@ -56,7 +56,6 @@ try:
     influxDB_tag_source = config['influxDB']['tag_source']
 
     serial_port = config['modbus']['port']
-    slave_address = config['modbus']['port']
 
     mqtt_broker_address = config['mqtt']['broker_address']
     mqtt_client_name = str(socket.gethostname()+sys.argv[0])
@@ -86,14 +85,17 @@ try:
     points = []
 
     # Modbus registers
-    reg =  [[  16, 0, True, 'wp_modus_betriebsart',         'mode',     'Betriebsart'],                         # 0=Aus, 1=EcoSommer, 2=EcoWinter, 3=Komfort
-            [  62, 0, '',   'wp_on-off_kuehlung',           'switch',   'Kühlung an/aus'],                      # 0=Aus, 1=An
-            [2001, 0, True, 't300_on-off_heizstab',         'switch',   'Heizstab an/aus'],                     # 0=Aus, 1=An
-            [  70, 1, True, 'wp_soll-temp_zone1',           'temp',     'Wohnen  (Zone1) Soll-Temperatur'],     # 100 - 295  ##-0,5 Abweichung zur Anzeige in der Anlage
-            [  75, 1, True, 'wp_soll-temp_zone2',           'temp',     'Büro KG (Zone2) Soll-Temperatur'],     # 100 - 295
-            [2000, 1, True, 't300_soll-temp_wasser',        'temp',     'Wasser Soll-Temperatur'],              # 450 - 600
-            [2003, 1, True, 't300_schwelle-temp_wasser',    'temp',     'Wasser Temperatur-Schwelle Heizstab'], # 400 - 500
-            [ 133, 0, True, 'wp_restzeit_intensivlueftung', 'time',     'Intensivlüftung Restzeit']]            # 0 - 1440
+    # - write_register(registeraddress: int, value: Union[int, float], number_of_decimals: int = 0, functioncode: int = 16, signed: bool = False)
+    # - read_register(registeraddress: int, number_of_decimals: int = 0, functioncode: int = 3, signed: bool = False)
+    #        reg-nr, nr-decimals-read, nr-decimals-write, functioncode, signed,             device/measurement, type (HA), comment
+    reg =  [[    16,                0,                 0,            3,   True,         'wp_modus_betriebsart',    'mode', 'Betriebsart'],                         # 0=Aus, 1=EcoSommer, 2=EcoWinter, 3=Komfort
+            [    62,                0,                 0,            3,     '',           'wp_on-off_kuehlung',  'switch', 'Kühlung an/aus'],                      # 0=Aus, 1=An
+            [  2001,                0,                 0,            3,   True,         't300_on-off_heizstab',  'switch', 'Heizstab an/aus'],                     # 0=Aus, 1=An
+            [    70,                2,                 1,            3,   True,           'wp_soll-temp_zone1',    'temp', 'Wohnen  (Zone1) Soll-Temperatur'],     # 100 - 295  ##-0,5 Abweichung zur Anzeige in der Anlage
+            [    75,                2,                 1,            3,   True,           'wp_soll-temp_zone2',    'temp', 'Büro KG (Zone2) Soll-Temperatur'],     # 100 - 295
+            [  2000,                1,                 1,            3,   True,        't300_soll-temp_wasser',    'temp', 'Wasser Soll-Temperatur'],              # 450 - 600
+            [  2003,                1,                 1,            3,   True,    't300_schwelle-temp_wasser',    'temp', 'Wasser Temperatur-Schwelle Heizstab'], # 400 - 500
+            [   133,                0,                 0,            3,   True, 'wp_restzeit_intensivlueftung',    'time', 'Intensivlüftung Restzeit']]            # 0 - 1440
 
 except Exception as e:
     logger.debug(e)
@@ -104,9 +106,75 @@ except Exception as e:
 
 # when connecting to mqtt do this;
 def on_connect(client, userdata, flags, rc):
+    locked = True
+    while unlocked == True:
+        try:
+            logger.debug("Lock schleife start")
+            lock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            logger.debug("Lock schleife bind")
+            lock.bind( '\0postconnect_gateway_notify_lock') 
+            locked = False
+
+        except socket.error as e:
+            logger.debug(e)
+            delay = random.randint(5, 15)
+            logger.info("Process is already running. Waiting "+str(delay)+" seconds")
+            time.sleep(delay)
+
+    else:
+        logger.info("No other processes running. Going forward")
+
     try:
         logger.info("Connected with result code "+str(rc))
         client.publish(mqtt_topic_debug,"Devive "+str(mqtt_client_name)+" connected with result code "+str(rc))
+
+        points = []
+
+        for i in range(len(reg)):
+            # subscribe to command topic
+            mqtt_command_topic = mqtt_topic_prefix+"/"+str(reg[i][6])+"/"+str(reg[i][5])+"/command"
+            client.subscribe(mqtt_command_topic)
+
+            # update state topic
+            mqtt_state_topic = mqtt_topic_prefix+"/"+str(reg[i][6])+"/"+str(reg[i][5])+"/state"
+            mqtt_state_topic = mqtt_topic_prefix+"/"+str(reg[i][6])+"/"+str(reg[i][5])+"/state"
+            logger.debug("state_topic - "+mqtt_state_topic)
+            logger.debug("Register Array - "+str(reg[i]))
+            value = instr.read_register(reg[i][0],reg[i][1],reg[i][3],True)
+            logger.debug("Value: "+str(value))
+
+            if reg[i][5] == 'wp_soll-temp_zone1':
+                value = value + 0.5
+                logger.debug("Value: "+str(value))
+
+            client.publish(mqtt_state_topic,str(value),qos=0,retain=True)
+
+            logger.info(str(reg[i][7]).ljust(35)+": "+str(value)+" "+str(reg[i][6]))
+            point = {
+                "measurement": str(reg[i][5]),
+                "tags": {
+                    "instance": influxDB_tag_instance,
+                    "source": influxDB_tag_source
+                },
+                #   "time": timestamp,   # Wenn nicht genutzt, wird der aktuelle Timestamp aus influxDB genutzt
+                "fields": {
+                    str(reg[i][6]): value
+                    }
+                }
+            logger.debug(str(point))
+            points.append(point)
+
+            # update availability topic
+            mqtt_availability_topic = mqtt_topic_prefix+"/"+str(reg[i][6])+"/"+str(reg[i][5])+"/availability"
+            client.publish(mqtt_availability_topic,"online",qos=0,retain=True)
+
+            client.publish(mqtt_topic_debug,"Subscribed to topic: "+str(mqtt_availability_topic))
+
+        clientInfluxDB.write_points(points)
+
+        logger.info("Closed running processes")
+        lock.close()
+
     except Exception as e:
         logger.debug(e)
         sys.exit(2)
@@ -114,6 +182,24 @@ def on_connect(client, userdata, flags, rc):
 
 # when receiving a mqtt message do this;
 def on_message(client, userdata, message):
+    locked = True
+    while unlocked == True:
+        try:
+            logger.debug("Lock schleife start")
+            lock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            logger.debug("Lock schleife bind")
+            lock.bind( '\0postconnect_gateway_notify_lock') 
+            locked = False
+
+        except socket.error as e:
+            logger.debug(e)
+            delay = random.randint(5, 15)
+            logger.info("Process is already running. Waiting "+str(delay)+" seconds")
+            time.sleep(delay)
+
+    else:
+        logger.info("No other processes running. Going forward")
+
     try:
         logger.info("Message received="+str(message.payload.decode("utf-8")))
         logger.info("Message topic="+str(message.topic))
@@ -130,37 +216,40 @@ def on_message(client, userdata, message):
         # MQTT Number     -> temp, time
 
         for i in range(len(reg)):
-            if device == reg[i][3]:
+            if device == reg[i][5]:
                 if reg[i][0] == 62:
                     if value == 1:
                         logger.info("Write register="+str(reg[i][0])+" Value="+str(value))
-                        instr.write_register(reg[i][0], 1, reg[i][1], 6, True)
+                        instr.write_register(reg[i][0], 1, reg[i][2], 6, True)
                         
                     if value == 0:
                         logger.info("Write register="+str(reg[i][0])+" Value="+str(value))
-                        instr.write_register(reg[i][0], 0, reg[i][1], 6, False)
+                        instr.write_register(reg[i][0], 0, reg[i][2], 6, False)
                 
                 else:
                     logger.info("Write register="+str(reg[i][0])+" Value="+str(value))
-                    instr.write_register(reg[i][0], value, reg[i][1], 6, reg[i][2])
-            
-            point = {
-                "measurement": str(reg[i][3]),
-                "tags": {
-                    "instance": influxDB_tag_instance,
-                    "source": influxDB_tag_source
-                },
-                #   "time": timestamp,   # Wenn nicht genutzt, wird der aktuelle Timestamp aus influxDB genutzt
-                "fields": {
-                    str(reg[i][4]): value
-                    }
-                }
-            logger.debug(str(point))
-            points.append(point)
+                    instr.write_register(reg[i][0], value, reg[i][2], 6, reg[i][3])
 
-            break
+                client.publish(mqtt_topic_prefix+"/"+reg[i][6]+"/"+reg[i][5]+"/state",str(value),qos=0,retain=True)
+            
+                point = {
+                    "measurement": str(reg[i][5]),
+                    "tags": {
+                        "instance": influxDB_tag_instance,
+                        "source": influxDB_tag_source
+                    },
+                    #   "time": timestamp,   # Wenn nicht genutzt, wird der aktuelle Timestamp aus influxDB genutzt
+                    "fields": {
+                        str(reg[i][6]): value
+                        }
+                    }
+                logger.debug(str(point))
+                points.append(point)
         
         clientInfluxDB.write_points(points)
+
+        logger.info("Closed running processes")
+        lock.close()
 
     except Exception as e:
         logger.debug(e)
